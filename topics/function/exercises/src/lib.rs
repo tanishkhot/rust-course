ab99b6c35438e68491b2f99aca3690891e4cb1cc
Tanish Khot <tanishkhot@protonmail.com>
2025-10-06 10:39:26 +0530

    exec: functions

--- topics/function/exercises/src/lib.rs
+++ topics/function/exercises/src/lib.rs
@@ -1,7 +1,15 @@
-pub fn mul() {
-    todo!();
+pub fn mul(x: u32, y: u32) -> u32 {
+    x * y
 }
 
-pub fn div() {
-    todo!();
+pub fn div(x, y) -> u32 {
+    x / y
+}
+
+fn main() {
+    let x: u32 = 3;
+    let y: u32 = 5;
+
+    println!("Multiplication of {x} and {y} is : {}", mul(x, y));
+    println!("Division of {x} and {y} is : {}", div(x, y));
 }
